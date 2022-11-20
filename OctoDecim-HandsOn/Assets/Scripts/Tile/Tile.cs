674cd56bb81ccc56f543afc15b5ec7ad912117a6
Benth48 <benilesmana48@gmail.com>
2022-11-20 22:36:30 +0700

    Map
    Just map without obstacle or mechanics
    maybe tomorrow i will make a mechanics and obstacle

---
+++ OctoDecim-HandsOn/Assets/Scripts/Tile/Tile.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    [SerializeField] private GameObject _highlight;
+
+    void OnMouseEnter() {
+        _highlight.SetActive(true);
+    }
+    void OnMouseExit() {
+        _highlight.SetActive(false);
+    }
+}
