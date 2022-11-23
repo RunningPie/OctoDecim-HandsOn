57455aa4aee8ff67fb04c700c0f4a9a0ea8c02be
Dama Dhananjaya Daliman <dama.dhananjaya.daliman@gmail.com>
2022-11-23 23:47:41 +0700

    Added Base HUD and Wave System
    What was added:
    1) Wave / Stage System
    2) Enemy and Player Turns
    3) Enemy and Player Attack Functions
    4) Basic HUD
    5) Enemy Spawn
    
    To Add:
    1)  Item Drops
    2) Health and Movement System
    3) Animations
    4) Enemy pathfinding
    5) Starting screen
    6) Win / Final Screen
    7) Scoring
    8) Audio
    
    To FIX:
    1) Random enemy spawn

--- OctoDecim-HandsOn/Assets/Scripts/BattleSystem.cs
+++ OctoDecim-HandsOn/Assets/Scripts/BattleSystem.cs
@@ -23,18 +23,26 @@ public class BattleSystem : MonoBehaviour
     Unit playerUnit;
     Unit enemyUnit;
 
-    TextMeshProUGUI enemyName;
-    TextMeshProUGUI playerName;
+    // TextMeshProUGUI enemyName;
+    // TextMeshProUGUI playerName;
+    public TextMeshProUGUI actionText;
+    public Button attackBtn;
+    public Button moveBtn;
 
     // Start is called before the first frame update
     void Start()
     {
         state = BattleState.START;      // Set BattleSystem state to START
-        SetupBattle();
+        StartCoroutine(SetupBattle());
+        
     }
 
-    void SetupBattle()
+    IEnumerator SetupBattle()
     {
+        actionText.gameObject.SetActive(false);
+        moveBtn.gameObject.SetActive(false);
+        attackBtn.gameObject.SetActive(false);
+
         // Grab player game object reference
         GameObject playerGO = Instantiate(playerPrefab, playerPosition);
         playerUnit = playerGO.GetComponent<Unit>();
@@ -43,13 +51,114 @@ public class BattleSystem : MonoBehaviour
         GameObject enemyGO = Instantiate(enemyPrefab, enemyPosition);
         enemyUnit = enemyGO.GetComponent<Unit>();
 
-        // Display enemy name on TMP GUI
-        enemyName = enemyGO.transform.GetChild(1).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
-        enemyName.text = enemyUnit.unitName;
+        // // Display enemy name on TMP GUI
+        // enemyName = enemyGO.transform.GetChild(1).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+        // enemyName.text = enemyUnit.unitName;
 
-        // Display player name on TMP GUI
-        playerName = playerGO.transform.GetChild(1).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
-        playerName.text = playerUnit.unitName;
+        // // Display player name on TMP GUI
+        // playerName = playerGO.transform.GetChild(1).GetChild(0).gameObject.GetComponent<TextMeshProUGUI>();
+        // playerName.text = playerUnit.unitName;
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.PLAYERTURN;
+        PlayerTurn();
     }
 
+    IEnumerator PlayerAttack()
+    {
+        // DAMAGE ENEMY
+        Debug.Log("Hit Enemy");
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+        yield return new WaitForSeconds(2f);
+
+        // CHECK IF ENEMY IS DEAD
+        if (isDead)
+        {
+            // END BATTLE
+            state = BattleState.WON;
+            EndBattle();
+        }
+        else
+        {
+            // ENEMY TURN
+            state = BattleState.ENEMYTURN;
+            StartCoroutine(EnemyTurn());
+        }
+
+    }
+
+    IEnumerator EnemyTurn()
+    {
+        Debug.Log("Enemy is attacking");
+
+        yield return new WaitForSeconds(1f);
+
+        bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+        
+        yield return new WaitForSeconds(1f);
+
+        if (isDead)
+        {
+            state = BattleState.LOST;
+            EndBattle();
+        }
+        else
+        {
+            state = BattleState.PLAYERTURN;
+            PlayerTurn();
+        }
+
+    }
+
+    IEnumerator PlayerHeal()
+    {
+        playerUnit.Heal(5);
+
+        yield return new WaitForSeconds(2f);
+
+        state = BattleState.ENEMYTURN;
+        StartCoroutine(EnemyTurn());
+    }
+
+    void EndBattle()
+    {
+        if (state == BattleState.WON)
+        {
+            Debug.Log("You Won");
+        }
+        else if (state == BattleState.LOST)
+        {
+            Debug.Log("You Lost");
+        }
+    }
+
+    void PlayerTurn()
+    {
+        Debug.Log("Players Turn");
+        actionText.gameObject.SetActive(true);
+        moveBtn.gameObject.SetActive(true);
+        attackBtn.gameObject.SetActive(true);
+
+    }
+
+    public void OnAttackButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+
+        StartCoroutine(PlayerAttack());
+    }
+
+    public void OnHealButton()
+    {
+        if (state != BattleState.PLAYERTURN)
+        {
+            return;
+        }
+
+        StartCoroutine(PlayerHeal());
+    }
 }
---
+++ OctoDecim-HandsOn/Assets/Scripts/WaveSpawner.cs
@@ -0,0 +1,136 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public enum SpawnState { SPAWNING, WAITING, COUNTING };
+
+    [System.Serializable]
+    public class Wave
+    {
+        public string name;
+        public Transform enemy;
+        public int enemyCount;
+        public float rate;
+
+
+    }
+
+    public Transform[] spawnPoints;
+
+    public Wave[] waves;
+    private int nextWave = 0;
+
+    public float timeBetweenWaves = 5f;
+    public float waveCountDown;
+
+    private float searchCountDown = 1f;
+
+    public SpawnState state = SpawnState.COUNTING;
+
+    public bool doneSpawning;
+
+    void Start()
+    {
+        waveCountDown = timeBetweenWaves;
+
+        if (spawnPoints.Length == 0)
+        {
+            Debug.Log("NO Spawn Point Referenced");
+        }
+    }
+
+
+    void Update()
+    {
+        if (state == SpawnState.WAITING)
+        {
+            // CHECK IF ENEMIES ARE STILL ALIVE
+            if (!EnemyIsAlive())
+            {
+                // BEGIN NEW WAVE
+                WaveCompleted();
+                return;
+
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        if (waveCountDown <= 0)
+        {
+            if (state != SpawnState.SPAWNING)
+            {
+                // Start Spawning Wave
+                StartCoroutine(SpawnWave(waves[nextWave]));
+            }
+        }
+        else
+        {
+            waveCountDown -= Time.deltaTime;
+        }
+    }
+
+    void WaveCompleted()
+    {
+        Debug.Log("Wave Completed");
+
+        state = SpawnState.COUNTING;
+        waveCountDown = timeBetweenWaves;
+
+        if (nextWave + 1 > waves.Length - 1)
+        {
+            nextWave = 0;
+            Debug.Log("Completed all waves. Looping...");
+        }
+        else
+        {
+            nextWave++;
+        }
+
+    }
+
+    bool EnemyIsAlive()
+    {
+        searchCountDown -= Time.deltaTime;
+        if (searchCountDown <= 0){
+            searchCountDown = 1f;
+            if (GameObject.FindGameObjectWithTag("Enemy") ==  null)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    IEnumerator SpawnWave(Wave _wave)
+    {
+        state = SpawnState.SPAWNING;
+
+        // SPAWN WAVE
+        Debug.Log("Spawning Wave: " + _wave.name);
+        for (int i = 0; i < _wave.enemyCount; i++)
+        {
+            SpawnEnemy(_wave.enemy);
+            yield return new WaitForSeconds(0f);
+        }
+
+        
+        state = SpawnState.WAITING;
+
+        yield break;
+    }
+
+    void SpawnEnemy(Transform _enemy)
+    {
+        // SPAWN ENEMY
+        Debug.Log("Spawning Enemy: "+_enemy.name);
+
+        Transform _sp = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        Instantiate (_enemy, _sp.position, _sp.rotation);
+        
+    }
+}
